890c305ac9f4c2f8fe3f5010cb8de068a1713378
vikastiwari44455-boop <vikastiwari44455@gmail.com>
2026-02-11 23:09:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request
+import requests
+from config import ACCESS_TOKEN, PHONE_NUMBER_ID, VERIFY_TOKEN
+from ai import ask_ai
+import os
+
+app = Flask(__name__)
+
+def send_message(to, text):
+    url = f"https://graph.facebook.com/v19.0/{PHONE_NUMBER_ID}/messages"
+
+    headers = {
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "messaging_product": "whatsapp",
+        "to": to,
+        "type": "text",
+        "text": {"body": text}
+    }
+
+    requests.post(url, headers=headers, json=data)
+
+
+@app.route("/webhook", methods=["GET"])
+def verify():
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+
+    if token == VERIFY_TOKEN:
+        return challenge
+    return "Error"
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+
+    try:
+        msg = data["entry"][0]["changes"][0]["value"]["messages"][0]
+        sender = msg["from"]
+        text = msg["text"]["body"]
+
+        reply = ask_ai(text)
+
+        send_message(sender, reply)
+
+    except Exception as e:
+        print("Error:", e)
+
+    return "OK", 200
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
